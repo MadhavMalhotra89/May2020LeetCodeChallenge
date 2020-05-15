68bf899282bc5102a41059daa5492725feca3cc3
MadhavMalhotra89 <55325147+MadhavMalhotra89@users.noreply.github.com>
2020-05-15 21:05:31 +0530

    Create Maximum Sum Circular Subarray.py

---
+++ Maximum Sum Circular Subarray.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxSubarraySumCircular(self, A):
+        total, maxSum, curMax, minSum, curMin = 0, -float('inf'), 0, float('inf'), 0
+        for a in A:
+            curMax = max(curMax + a, a)
+            maxSum = max(maxSum, curMax)
+            curMin = min(curMin + a, a)
+            minSum = min(minSum, curMin)
+            total += a
+        return max(maxSum, total - minSum) if maxSum > 0 else maxSum
